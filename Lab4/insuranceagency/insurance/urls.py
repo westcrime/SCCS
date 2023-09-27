fa8941cb7fc44eaef689d68cbc6c9bb91e950bba
westcrime <wesstcrime77@rambler.com>
2023-09-27 15:54:13 +0300

    update

--- Lab4/insuranceagency/insurance/urls.py
+++ Lab4/insuranceagency/insurance/urls.py
@@ -4,12 +4,10 @@ from .views import *
 urlpatterns = [
                 path('', InsuranceCategoriesPage.as_view(), name='home'),
                 path('logout/', logout_user_link, name='logout'),
-                path('', InsuranceCategoriesPage.as_view(), name='make_contract'),
                 path('activate_contract/', activate_contract_link, name='activate_contract'),
                 path('delete_object/', delete_object_link, name='delete_object'),
                 path('edit_object/', edit_object, name='edit_object'),
                 path('add_object/', AddObject.as_view(), name='add_object'),
-                path('', InsuranceCategoriesPage.as_view(), name='contact'),
                 path('register/', RegisterUser.as_view(), name='register'),
                 path('login/', LoginUser.as_view(), name='login'),
                 path('insurance_branches/', InsuranceBranchesPage.as_view(), name='insurance_branches'),
